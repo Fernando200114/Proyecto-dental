eec7d7ae4b93582b3221bc0241774d8326b66a09
Fernando200114 <fernandojosellullunacuichan@gmail.com>
2026-03-11 20:11:10 -0500

    DebugTrue

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xotzkq2$lcupncy=-2&&#h1k9gjg(d*r-c_-3)-eh$-vebhx(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["4.227.103.223","localhost","127.0.0.1"]
 # Application definition
